03f63e1a94c02134060b35af86bf2121b7ff5a60
bdawson1993 <b.dawson1993@gmail.com>
2019-02-12 09:45:19 +0000

    Added shape class

---
+++ PhysicsEngine/CatapultArm.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Actor.h"
+
+class CatapultArm : public Actor
+{
+    public:
+        void CatapultArm(string name, PxPhysics* phy) : Actor(name, phy)
+        {
+
+        };
+
+
+
+};
+
+
