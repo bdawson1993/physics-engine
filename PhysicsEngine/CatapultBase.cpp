c75d25a904d1be32d1ceef7a918167fd7afc84e9
bdawson1993 <b.dawson1993@gmail.com>
2019-02-12 13:39:14 +0000

    Latest Changes

--- PhysicsEngine/CatapultBase.cpp
+++ PhysicsEngine/CatapultBase.cpp
@@ -5,6 +5,7 @@ void CatapultBase::CreateDynamic()
 {
 	PxMaterial* mat = physics->createMaterial(1, 1, 1);
 	actor = (PxRigidDynamic*)physics->createRigidDynamic(PxTransform(PxVec3(0, 0, 0)));
+	
 
 	//left side
 	PxShape* left = physics->createShape(PxBoxGeometry(0.5f, 0.5f, 2.0f), *mat);
@@ -24,6 +25,7 @@ void CatapultBase::CreateDynamic()
 	PxShape* back = physics->createShape(PxBoxGeometry(3.0f, 0.5f, 0.5f), *mat);
 	back->setLocalPose(PxTransform(PxVec3(2.5f, 0.0f, -2.5f)));
 	actor->attachShape(*back);
+
 	
 
 	
