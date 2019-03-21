b65abf71c39eae5d8c5d0b919a60d7201fe4d195
bdawson1993 <b.dawson1993@gmail.com>
2019-03-21 09:57:10 +0000

    Added Timer provided by oliver

---
+++ PhysicsEngine/SZ_HighResTimer.cpp
@@ -0,0 +1,14 @@
+#include "SZ_HighResTimer.h"
+SZ_HighResTimer::SZ_HighResTimer()
+{
+}
+void SZ_HighResTimer::resetChronoTimer()
+{
+	startChrono = Clock::now();
+}
+float SZ_HighResTimer::getChronoTime()
+{
+	std::chrono::steady_clock::time_point now = Clock::now();
+	auto timeDiff = std::chrono::duration_cast<std::chrono::nanoseconds>(now - startChrono).count();
+	return (float)timeDiff;
+}
---
+++ PhysicsEngine/SZ_HighResTimer.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <chrono>
+//Written by Olivier Szymanezyk oszymanezyk@lincoln.ac.uk
+class SZ_HighResTimer
+{
+private:
+	typedef std::chrono::high_resolution_clock Clock;
+	std::chrono::steady_clock::time_point startChrono;
+
+public:
+	SZ_HighResTimer();
+	void resetChronoTimer();
+	float getChronoTime();
+};
+
